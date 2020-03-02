68f39358dff6fcf4a22b57ac0249bca1fcd62c94
Zubair <zubairali78@gmail.com>
2020-03-02 16:28:49 -0500

    modified add address

--- src/test/java/com/target/account/LogInTest.java
+++ src/test/java/com/target/account/LogInTest.java
@@ -2,7 +2,9 @@ package com.target.account;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class LogInTest extends BaseTest{
 
@@ -19,7 +21,25 @@ public class LogInTest extends BaseTest{
         loginTest();
         driver.get("https://www.boxed.com/account/address");
         driver.findElementByXPath("//*[@id='address-list-v2']//button").click();
-        Thread.sleep(6000);
+        Thread.sleep(3000);
+        driver.findElementByXPath("//input[@name='firstName']").sendKeys("Asad");
+        driver.findElementByXPath("//input[@name='lastName']").sendKeys("Zaman");
+        driver.findElementById("addressLine1").sendKeys("131 Francis Ave");
+        driver.findElementById("city").sendKeys("Buffalo");
+        Select state = new Select(driver.findElementById("state"));
+        state.selectByValue("NY");
+        driver.findElementById("postalCode").sendKeys("14212", Keys.RETURN);
+        Thread.sleep(2000);
+
+
+    }
+
+    @Test
+    public void removeAddressTest() throws InterruptedException {
+        loginTest();
+        driver.get("https://www.boxed.com/account/address");
+        driver.findElementByXPath("//*[text() = 'Remove']").click();
+        Thread.sleep(5000);
 
     }
 
